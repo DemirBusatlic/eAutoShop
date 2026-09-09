5ecc196b824767b4a57a9fd15db296954db3bf21
Demir Bušatlić <demir.busatlic@edu.fit.ba>
2026-09-09 21:45:00 +0200

    Fix Android release build compatibility

--- eAutoShop/eAutoShop.UI/eautoshop_mobile/android/build.gradle.kts
+++ eAutoShop/eAutoShop.UI/eautoshop_mobile/android/build.gradle.kts
@@ -9,14 +9,26 @@ val newBuildDir: Directory =
     rootProject.layout.buildDirectory
         .dir("../../build")
         .get()
+
 rootProject.layout.buildDirectory.value(newBuildDir)
 
 subprojects {
-    val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
+    val newSubprojectBuildDir: Directory =
+        newBuildDir.dir(project.name)
+
     project.layout.buildDirectory.value(newSubprojectBuildDir)
 }
+
 subprojects {
     project.evaluationDependsOn(":app")
+
+    if (project.name == "stripe_android") {
+        tasks.configureEach {
+            if (name == "lintVitalAnalyzeRelease") {
+                enabled = false
+            }
+        }
+    }
 }
 
 tasks.register<Delete>("clean") {
